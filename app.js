e2ec43d56e0a787bf0c8bab428dd161b58003333
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-02-22 09:25:25 +0100

    adding methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Becoming a vue ninja'
+    title: 'Becoming a vue ninja',
+    name: 'John Pelling'
+  },
+  methods: {
+    greet(time) {
+      return `Hello ${this.name} ,  good ${time}`
+    }
   }
 })
