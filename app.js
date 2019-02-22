175d9a16aef5a70f5b0f03717bfd5e6d8147a42c
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-02-22 10:14:41 +0100

    bind vue event listeners

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Becoming a vue ninja',
-    name: 'John Pelling',
-    url: 'www.youtube.com',
-    classes: ['one', 'two']
+    wage: 10
   },
   methods: {
-    greet(time) {
-      return `Hello ${this.name} ,  good ${time}`
-    }
+   increaseWage(amount){
+     this.wage += amount;
+     good
+   }
   }
 })
